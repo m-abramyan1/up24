3f5ab5a7df735021f40952a5bd2531554ab87151
Manuk Abramyan <m.abramyan@MacBook-Pro-Manuk.local>
2021-07-19 15:57:08 +0300

    add animation

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -1,31 +1,24 @@
 "use strict";
 
-// Создаем функицию которая при клике на кнопку отправляет GET запрос 
-function sendRequest() {
-  // Создаем переменные для получения и хранения значения из Input
-  var nameForm = document.getElementById('name').value; // Переменная с именем из input с id name
-
-  var messageForm = document.getElementById('number').value; // Переменная с сообщением из input с id message
-
-  var massage = "\u0418\u043C\u044F: ".concat(nameForm, " \xA0\u0421\u043E\u043E\u0431\u0449\u0435\u043D\u0438\u0435: ").concat(messageForm); // Переменная с сообщением для отправки в телеграмм 
-  // Переменная с get запросом 
-
-  /*
-      Все запросы к Telegram Bot API должны осуществляться через HTTPS в следующем виде: 
-      https://api.telegram.org/bot<token>/sendMessage?chat_id=<...>&text=<сообщение для отправки>
-       token - 
-      sendMessage - название метода (в нашем случае отправка сообщения)
-      chat_id - id чата в который будет отправлено сообщение
-   */
-
-  var getInquiry = "https://api.telegram.org/bot1664447266:AAEgRGF4_s2p-iVDbgnJiXa0I3dj_qHqe-U/sendMessage?chat_id=-560960287&text=".concat(massage); // Cоздание объекта XMLHttpRequest
-
-  var xhr = new XMLHttpRequest(); //true или не определён, запрос выполнится асинхронно, в противном случае — синхронно. 
-
-  xhr.open("GET", getInquiry, true); // Отправляем get запрос
-
-  xhr.send(null); // Очищаемя поля для ввода данных
-
-  document.getElementById("name").value = "";
-  document.getElementById("number").value = "";
-}
+var skroll = new Skroll().add(".home__title", {
+  animation: "zoomIn",
+  duration: 600
+}).add(".work__item", {
+  animation: "fadeInUp",
+  delay: 120,
+  duration: 600,
+  wait: 250
+}).add(".anim3", {
+  animation: "fadeInUp ",
+  delay: 100,
+  duration: 750
+}).add(".anim", {
+  animation: "fadeInUp ",
+  delay: 100,
+  duration: 750
+}).add(".about__img", {
+  animation: "growInLeft",
+  delay: 80,
+  duration: 500,
+  easing: "cubic-bezier(0.37, 0.27, 0.24, 1.26)"
+}).init();
--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
- // Создаем функицию которая при клике на кнопку отправляет GET запрос 
- function sendRequest() {
-    // Создаем переменные для получения и хранения значения из Input
-    let nameForm = document.getElementById('name').value // Переменная с именем из input с id name
-    let messageForm = document.getElementById('number').value // Переменная с сообщением из input с id message
-    let massage = `Имя: ${nameForm}  Сообщение: ${messageForm}` // Переменная с сообщением для отправки в телеграмм 
-    // Переменная с get запросом 
-    /*
-        Все запросы к Telegram Bot API должны осуществляться через HTTPS в следующем виде: 
-        https://api.telegram.org/bot<token>/sendMessage?chat_id=<...>&text=<сообщение для отправки>
-
-        token - 
-        sendMessage - название метода (в нашем случае отправка сообщения)
-        chat_id - id чата в который будет отправлено сообщение
-     */
-    let getInquiry = `https://api.telegram.org/bot1664447266:AAEgRGF4_s2p-iVDbgnJiXa0I3dj_qHqe-U/sendMessage?chat_id=-560960287&text=${massage}`
-    // Cоздание объекта XMLHttpRequest
-    let xhr = new XMLHttpRequest();
-    //true или не определён, запрос выполнится асинхронно, в противном случае — синхронно. 
-    xhr.open("GET", getInquiry, true); 
-    // Отправляем get запрос
-    xhr.send(null);
-    // Очищаемя поля для ввода данных
-    document.getElementById("name").value = "";
-    document.getElementById("number").value = "";
-  }
+var skroll = new Skroll()
+   .add(".home__title",{
+      animation:"zoomIn",
+      duration:600
+   })
+   .add(".work__item",{
+      animation:"fadeInUp",
+      delay:120,
+      duration:600,
+      wait:250
+   })
+   .add(".anim3",{
+      animation:"fadeInUp ",
+      delay:100,
+      duration:750
+   })
+   .add(".anim",{
+      animation:"fadeInUp ",
+      delay:100,
+      duration:750
+   })
+   .add(".about__img",{
+      animation:"growInLeft",
+      delay:80,
+      duration:500,
+      easing:"cubic-bezier(0.37, 0.27, 0.24, 1.26)"
+   })
+   .init();
